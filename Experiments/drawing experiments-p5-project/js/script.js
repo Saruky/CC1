aceb5b504ec3d32a70c9a3e19041784d523432d5
Saruky <74262335+Saruky@users.noreply.github.com>
2021-09-16 10:36:33 -0400

    Modes: CENTER

--- Experiments/drawing experiments-p5-project/js/script.js
+++ Experiments/drawing experiments-p5-project/js/script.js
@@ -1,6 +1,6 @@
 /**
-Title of Project
-Author Name
+Title of Project: Drawing
+Author Name: Suzana Burlea
 
 This is a template. You must fill in the title,
 author, and this description to match your project!
@@ -21,6 +21,18 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas (500, 500);
+  background (250, 200, 100);
+  point (250, 250);
+  rectMode(CENTER);
+  rect (250, 250, 300, 300);
+  rect (250, 250, 200, 200);
+  line (0, 0, 500, 500);
+  line(500, 0, 0, 500);
+  ellipseMode (CORNER);
+  ellipse(250, 250, 250, 250);
+  ellipse(250, 250, 200, 200);
+  ellipse(250, 250, 150, 150);
 
 }
 
