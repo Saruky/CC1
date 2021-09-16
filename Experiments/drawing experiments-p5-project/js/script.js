6b7b2a945e2aa86ffc7e19498871332418b566ed
Saruky <74262335+Saruky@users.noreply.github.com>
2021-09-16 11:09:25 -0400

    Color and Shades

--- Experiments/drawing experiments-p5-project/js/script.js
+++ Experiments/drawing experiments-p5-project/js/script.js
@@ -22,15 +22,25 @@ Description of setup
 */
 function setup() {
   createCanvas (500, 500);
-  background (250, 200, 100);
+  background (77,255,255);
   point (250, 250);
+
   rectMode(CENTER);
+
+  fill(255,25,255);
   rect (250, 250, 300, 300);
+  noStroke();
+  fill(255,25,265);
   rect (250, 250, 200, 200);
+
   line (0, 0, 500, 500);
   line(500, 0, 0, 500);
   ellipseMode (CORNER);
+
+  fill(255,40,285, 50);
   ellipse(250, 250, 250, 250);
+  stroke (221,255,51);
+  fill(255,80,300, 100);
   ellipse(250, 250, 200, 200);
   ellipse(250, 250, 150, 150);
 
