8135fe60e5c570a53d326f12fd161ae675739145
Saruky <74262335+Saruky@users.noreply.github.com>
2021-09-16 13:34:47 -0400

    Hanging Alien

--- Activities/02-draw-an-alien/js/script.js
+++ Activities/02-draw-an-alien/js/script.js
@@ -1,13 +1,9 @@
 /**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Title of Project: Alien
+Author Name: Suzana Burlea
 */
 
 "use strict";
-
 
 /**
 Description of preload
@@ -21,6 +17,45 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas (640, 480);
+
+  background (255, 87, 238);
+
+  noStroke();
+
+  //draw wings
+  fill(140, 25, 255, 90);
+  ellipse(320, 0, 600, 400);
+
+  //draw body
+  fill(178,190,181);
+  ellipse(320, 0, 400, 500);
+
+  //draw head
+  fill(143,153,146);
+  ellipse(320, 280, 120, 320);
+
+  //draw eyes
+
+  fill(0);
+  ellipse(290, 330, 40, 115);
+  fill(0);
+  ellipse(350, 330, 40, 115);
+
+  //draw nostrils
+  fill(0);
+  ellipse(310, 235, 10, 10);
+  fill(0);
+  ellipse(330, 235, 10, 10);
+
+  //draw mouth
+  stroke(255,51,85);
+  strokeWeight(10);
+  rectMode(CENTER);
+  rect(320, 200, 80, 20);
+
+
+
 
 }
 
