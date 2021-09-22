d7dfb2560b7951abb8142254cca0924fd082727b
Saruky <74262335+Saruky@users.noreply.github.com>
2021-09-22 11:38:06 -0400

    E1: Started exercise 1

---
+++ exercices/i-like-to-move-it/js/script.js
@@ -0,0 +1,31 @@
+/**
+I like to Move It Activity
+Suzana Burlea
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
