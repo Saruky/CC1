f658cf4e9f648babfa6581ccc0690fe3eaf183ac
Saruky <74262335+Saruky@users.noreply.github.com>
2021-09-23 08:48:32 -0400

    A3: Started the moving pictures activity

---
+++ Activities/03-moving-pictures/js/script.js
@@ -0,0 +1,33 @@
+/**
+Activity 3: The Moving Pictures
+Suzana Burlea
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
--- exercices/i-like-to-move-it/js/script.js
+++ exercices/i-like-to-move-it/js/script.js
@@ -6,7 +6,6 @@ Suzana Burlea
 
 "use strict";
 
-
 /**
 Description of preload
 */
@@ -14,18 +13,38 @@ function preload() {
 
 }
 
-
 /**
 Description of setup
 */
+let backgroundshade = 49;
+let circle = {
+  size : 250,
+  x : 0,
+  y : 125,
+  speed : 1};
+
+
 function setup() {
-
+  createCanvas (windowWidth, windowHeight);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  noStroke();
+  background(backgroundshade);
+
+// Draw a square in the centre of the canvas
+  rectMode(CENTER);
+  fill(129, 150, 49);
+  rect(width/2, height/2, 550, 450);
+
+  fill(129, 54, 49);
+  ellipse(circle.x,circle.y,circle.size);
+  circle.x += circle.speed;
+  circle.speed = circle.speed  + 0.5;
+
+
 
 }
