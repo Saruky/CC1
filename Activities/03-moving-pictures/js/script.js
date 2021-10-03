e38e559faa0d286e74754240cd6347dfb1121706
Saruky <74262335+Saruky@users.noreply.github.com>
2021-10-03 15:21:22 -0400

    Drawing Circles A and B. Problems with Y, circle A

--- Activities/03-moving-pictures/js/script.js
+++ Activities/03-moving-pictures/js/script.js
@@ -2,32 +2,71 @@
 Activity 3: The Moving Pictures
 Suzana Burlea
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+Two circles, the left one bigger and more transparent than the right, come in from either side of the screen, growing as they do so. They stop in the centre while still growing. The background goes from black to red.
 
+
+*/
 "use strict";
 
-
-/**
-Description of preload
-*/
 function preload() {
 
 }
+let backgroundshade = 230;
 
 
-/**
-Description of setup
-*/
+let circleA = {
+  size : 350,
+  x : 1000,
+  y : 305,
+  speed : 2.5,
+  fill:(47, 196, 255, 122)
+};
+let circleB = {
+  size : 90,
+  x : 0,
+  y : 305,
+  speed : 7,
+  fill: 169
+};
+
+
 function setup() {
-
+  createCanvas (windowWidth, windowHeight);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  noStroke();
+
+  background(backgroundshade);
+  backgroundshade = constrain(backgroundshade, 10, 190);
+  backgroundshade += 1;
+
+
+// Draw a square in the centre of the canvas
+  rectMode(CENTER);
+  fill(129, 150, 49);
+  rect(width/2, height/2, 750, 500);
+
+//Draw moving circle A
+  circleA.fill = random(103, 205);
+  circleA.x = constrain(circleA.x, width, width/2);
+  fill(circleA.fill);
+
+  ellipse(circleA.x,circleA.y,circleA.size);
+  circleA.speed = -5;
+  circleA.x += circleA.speed;
+
+
+//Draw moving circle B
+  circleB.fill = map(circleB.x, 0, width, 0, 133);
+  circleB.x = constrain(circleB.x, 0, width/2);
+  fill(circleB.fill);
+
+  ellipse(circleB.x,circleB.y,circleB.size);
+  circleB.x += circleB.speed;
 
 }
+//Draw moving circle B
